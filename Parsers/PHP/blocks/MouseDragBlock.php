e568879a21fbd70f42e8e513408c09fcd5eb5895
Rodrigo Brun <rodrigotbrun@gmail.com>
2016-06-10 04:03:17 -0300

    Add parser do MouseDragBlock

--- Parsers/PHP/blocks/MouseDragBlock.php
+++ Parsers/PHP/blocks/MouseDragBlock.php
@@ -2,13 +2,58 @@
 
 namespace Trixmote\Blocks;
 
+use Trixmote\Model\Point;
+
 /**
  * Class MouseDragBlock
  */
 class MouseDragBlock extends TypeBlock {
 
     const FORMAT = "/\[d:([0-9\.]+x[0-9\.]+|.)+\[.*\]\]/";
-    
+
+    /**
+     * @var Point Coordenada na tela, indicando o local do clique.
+     */
+    private $points = [];
+
+    /**
+     * @var string Botão do mouse usado no clique
+     */
+    private $mouseButton;
+
+    /**
+     * MouseClickBlock constructor.
+     *
+     * @param $bs Bloco de evento de clique
+     */
+    public function __construct($bs) {
+        parent::__construct($bs);
+        $mouseButton = [];
+        $points = [];
+
+        preg_match('/([0-9\.]+x[0-9\.]+\|*)+/', $bs, $points);
+
+        preg_match('/\[[a-zA-Z\-]+\]/', $bs, $mouseButton);
+
+        $points = reset($points);
+        $points = explode("|", $points);
+
+        $mouseButton = str_replace(['[', ']'], '', reset($mouseButton));
+
+        $this->points = array_map('\Trixmote\Blocks\MouseDragBlock::strToPoint', $points);
+        $this->mouseButton = $mouseButton;
+    }
+
+    /**
+     * @param $str String point
+     *
+     * @return Point coordenada da tela
+     */
+    public static function strToPoint($str) {
+        $e = explode('x', $str);
+        return new Point((double)$e[0], (double)$e[1]);
+    }
+
     /**
      * @return String Identificador do bloco
      */
